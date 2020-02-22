31a7d9b35c7cc84ca3388ca46eac444e1d43622e
Gurleen Sethi <sarusethi@rocketmail.com>
2020-02-22 14:19:16 -0500

    refactor: username and password validations.

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,11 +1,15 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   @IsNotEmpty()
   username: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
   password: string;
 }
