462ceccdb1e9bc9f8e947cc779aaf872e6894f4a
Gurleen Sethi <sarusethi@rocketmail.com>
2020-02-23 03:38:53 -0500

    feat: associate user with task.

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,15 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule,
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
